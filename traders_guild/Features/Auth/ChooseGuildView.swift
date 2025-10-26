150e1fedb15f17d2c8ec491a6c2a4115d44299e1
Al Hennessey <alhennessey92@gmail.com>
2025-10-26 18:57:52 +0000

    login stuff and guild selection

---
+++ traders_guild/Features/Auth/ChooseGuildView.swift
@@ -0,0 +1,7 @@
+//
+//  ChooseGuildView.swift
+//  traders_guild
+//
+//  Created by Al Hennessey on 26/10/2025.
+//
+/// Display to user to allow them to select a current guild from their joined guilds, used after login
